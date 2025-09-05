d69a168877fbcc446fe406d255ee35f178b91805
faizaansheikh <princefaizan800@gmail.com>
2025-09-05 22:33:43 +0500

    fs

--- src/app/components/Education.jsx
+++ src/app/components/Education.jsx
@@ -3,6 +3,12 @@ import { MainBtn, MainColor, MainLightColor } from '../utils'
 import { MdWork } from "react-icons/md";
 import { IoIosCode } from "react-icons/io";
 import { GiGraduateCap } from "react-icons/gi";
+import Image from 'next/image';
+import img from '../images/c1.png'
+import img1 from '../images/c2.png'
+import img2 from '../images/c3.png'
+import { TiTick } from "react-icons/ti";
+import { LiaCertificateSolid } from "react-icons/lia";
 function Education() {
     let arr = [
         {
@@ -28,6 +34,31 @@ function Education() {
         },
     ];
 
+
+    let cert = [
+
+        {
+            img: img1,
+            title: 'React',
+            title2: 'HackerRank',
+            date: '2023',
+            link: 'https://www.hackerrank.com/certificates/68f770a6bb56'
+        },
+        {
+            img: img,
+            title: 'Javascript Algorithms And Data Structures',
+            title2: 'freeCodeCamp',
+            date: '2022',
+            link: 'https://www.freecodecamp.org/certification/Faizaan_sheikh/javascript-algorithms-and-data-structures'
+        },
+        {
+            img: img2,
+            title: 'Problem Solving',
+            title2: 'HackerRank',
+            date: '2023',
+            link: 'https://www.hackerrank.com/certificates/93680aba4050'
+        }
+    ]
     return (
         <div style={{ backgroundColor: MainColor }} className="w-full h-auto flex flex-col items-center pb-12">
             {/* Heading */}
@@ -39,6 +70,7 @@ function Education() {
             </div>
 
             {/* Vertical Steps */}
+
             <div className="relative w-full sm:w-[80%] mt-6 md:mt-10 px-4 sm:px-0">
                 {/* Vertical line (only visible on medium+) */}
                 <div className="hidden sm:block absolute left-5 top-0 bottom-0 w-[1px] bg-gray-600"></div>
@@ -46,7 +78,7 @@ function Education() {
                 {arr.map((x, i) => (
                     <div
                         key={i}
-                        className="relative flex flex-col sm:flex-row items-start mb-10"
+                        className="relative flex flex-col sm:flex-row items-start mb-10 "
                     >
 
                         {/* Step Icon */}
@@ -55,7 +87,7 @@ function Education() {
                         </div>
 
                         {/* Box Content */}
-                        <div className="flex justify-between sm:ml-8 mt-4 sm:mt-0 bg-[#080711] border border-[#44444E] rounded-[10px] p-4 w-full">
+                        <div className="flex justify-between sm:ml-8 mt-4 sm:mt-0 bg-[#080711] border border-[#44444E] rounded-[10px] p-4 w-full shadow-lg hover:shadow-[#5d2bbbff] relative hover:bottom-2 transition-all duration-600 ease-in-out">
 
 
                             <div>
@@ -73,6 +105,41 @@ function Education() {
                         </div>
                     </div>
                 ))}
+            </div>
+
+            <div className='bg-[] w-full sm:w-[80%] px-6 md:px-0 h-[435px] flex items-center gap-6 flex-col lg:flex-row mt-22 mb-8'>
+                {cert.map((x, i) => (
+                    <div onClick={() => window.open(x.link)} key={i} className='cursor-pointer bg-[#0A0914] w-full h-full border border-[#44444E] rounded-[10px] shadow-lg hover:shadow-[#5d2bbbff] relative hover:bottom-2 transition-all duration-600 ease-in-out'>
+                        <div className='border border-[#44444E] w-full h-[200px] rounded-[10px] relative  overflow-hidden'>
+                            <Image
+                                src={x.img}
+                                alt="Developer at desk"
+                                className="object-cover w-full h-full rounded-[10px]"
+                            />
+
+                            {/* Overlay */}
+                            <div className="absolute inset-0 bg-black/10 hover:bg-black/60 transition-all duration-300 rounded-[10px]" />
+
+                        </div>
+                        <div className=' w-full h-[]'>
+                            <div className='pl-4 mt-4 '>
+                                <LiaCertificateSolid style={{ backgroundColor: MainLightColor }} className='rounded-[50px] p-2 mb-5' size={55} color={MainBtn} />
+
+                                <p className='text-lg my-1 font-bold'>{x.title}</p>
+                                <p className='text-lg my-1' style={{ color: MainBtn }}>{x.title2}</p>
+                             {(i === 0 || i === 2) &&    <br />}
+                                <div className='flex items-center gap-2 my-2'>
+                                    <p className='text-sm text-[grey] '>{x.date}</p>
+                                    <span className='px-2 py-1 text-sm rounded-[13px] flex items-center gap-1' style={{ backgroundColor: MainLightColor, color: MainBtn }}>Verified <TiTick /></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+
+
+
+
             </div>
         </div>
     );
