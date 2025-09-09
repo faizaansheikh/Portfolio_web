2f687d05c1d00618d38f1267a441f471a74f0f08
faizaansheikh <princefaizan800@gmail.com>
2025-09-10 00:21:28 +0500

    fs

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,24 +2,25 @@ import React from 'react'
 import { MainBtn, MainColor, MainLightColor } from '../utils'
 import Navbar from './Navbar'
 import MyButton from './MyButton'
-
+import Image from 'next/image'
+import profile from '../images/about.jpg'
 function Main() {
 
   return (
     <>
-      
+
       <div style={{ backgroundColor: MainColor }} className='w-full h-[80px] flex justify-center'>
         <Navbar />
       </div>
 
-      
+
 
       <div style={{ backgroundColor: MainColor }} className='w-full h-[auto] py-20 md:py-26 flex justify-center'>
         <div className='bg-[] w-[89%] h-full flex justify-center items-center flex-col md:flex-row'>
           <div>
             <p className='text-white my-0  w-[auto] text-[17px] px-0 py-2 ' style={{ color: MainBtn }}>Welcome to my portfolio!</p>
             <h2 className='text-white text-6xl my-6'>Hi! My <br /> name's <span style={{ color: MainBtn }} className='font-bold '>Faizaan</span></h2>
-            <p className='text-[18px] text-[gray] my-2'>
+            <p className='text-[18px] text-[gray] my-2 pr-12'>
               Full Stack Developer crafting intuitive software that seamlessly connects real user needs with innovative technology.
               With a strong foundation in UI/UX design and end-to-end development,
               I bring bold ideas to life and deliver impactful, user-centric experiences.
@@ -32,7 +33,17 @@ function Main() {
 
           </div>
 
-         <div className='bg-[] w-full h-full flex flex-col items-center   justify-center'>sds</div>
+          <div className='bg-[] w-full h-full  flex flex-col items-center   justify-center mt-16 md:mt-0'>
+            <div className='w-[400px] h-[400px] rounded-[100px] '>
+              <Image
+                src={profile} // put your image inside public folder
+                alt="Developer at desk"
+                // fill
+                className="object-cover w-[100%] h-full rounded-[100px]"
+              // sizes="(max-width: 768px) 100vw, 320px"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MainColor, MainLightColor } from '../utils'
+import { MainBtn, MainColor, MainLightColor } from '../utils'
 import { MdDarkMode } from "react-icons/md";
 import ToggleTheme from './ToggleTheme';
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -7,7 +7,10 @@ function Navbar() {
     const navLists = [
         { label: 'Home' },
         { label: 'About' },
-        { label: 'Services' },
+        { label: 'Experience' },
+        { label: 'Skills' },
+        { label: 'Certifications' },
+        { label: 'Projects' },
         { label: 'Contact' },
     ]
     return (
@@ -20,9 +23,31 @@ function Navbar() {
                 <div className='flex items-center w-auto h-full'>
                     <ul className='flex items-center '>
                         {navLists.map((x, i) => (
-                            <li key={i} className='text-white text-[17px] px-6'>{x.label}</li>
+                            <li
+                                key={i}
+                                className="text-[lightgrey] text-[16px] px-4 cursor-pointer hover:text-[whitesmoke] hover:scale-105 group"
+                            >
+                                <span
+                                    style={{ color: MainBtn }}
+                                    className="font-bold pr-1 text-[18px] opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                                >
+                                    {"<"}
+                                </span>
+                                {x.label}
+                                <span
+                                    style={{ color: MainBtn }}
+                                    className="font-bold pl-1 text-[18px] opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                                >
+                                    {"/>"}
+                                </span>
+                            </li>
+
+
                         ))}
                     </ul>
+                    <div style={{ backgroundColor: MainLightColor }} className=' w-[80px] px-6 h-full flex items-center justify-center cursor-pointer'>
+                        <ToggleTheme />
+                    </div>
                 </div>
                 {/* <div className='bg-[blue] w-[90px] h-full flex items-center justify-center cursor-pointer'>
             <ToggleTheme/>
@@ -36,7 +61,7 @@ function Navbar() {
                     {/* <p className='text-white text-2xl pl-6'>Faizaan</p> */}
                 </div>
                 <div>
-                    <div className='bg-[] w-[90px] h-full flex items-center flex justify-center'><GiHamburgerMenu size={20}/></div>
+                    <div className='bg-[] w-[90px] h-full flex items-center flex justify-center'><GiHamburgerMenu size={20} /></div>
                 </div>
             </div>
         </>
