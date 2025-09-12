da53d4bb48be5c9bb913215d7292a2065f0d3e6a
faizaansheikh <princefaizan800@gmail.com>
2025-09-12 20:24:28 +0500

    fs

--- src/app/components/CustomCursor.jsx
+++ src/app/components/CustomCursor.jsx
@@ -1,43 +1,31 @@
-"use client";
-import { useTheme } from "next-themes";
-import React, { useEffect, useRef, useState } from "react";
+'use client'
+import { useTheme } from 'next-themes';
+import React, { useEffect, useRef } from 'react';
 
 const CustomCursor = () => {
-  const { theme, resolvedTheme } = useTheme(); // resolvedTheme handles "system"
-  console.log('fwefwe');
-  
-  const cursorRef = useRef(null);
-  const [mounted, setMounted] = useState(false);
+    const { theme } = useTheme();
+    const cursorRef = useRef(null);
+    console.log('dsadas');
+    
+    useEffect(() => {
+        const moveCursor = (e) => {
+            const { clientX, clientY } = e;
+            if (cursorRef.current) {
+                cursorRef.current.style.transform = `translate3d(${clientX - 10}px, ${clientY - 10}px, 0)`;
+            }
+        };
 
-  // Ensure hydration matches theme
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+        window.addEventListener('mousemove', moveCursor);
+        return () => window.removeEventListener('mousemove', moveCursor);
+    }, []);
 
-  useEffect(() => {
-    const moveCursor = (e) => {
-      const { clientX, clientY } = e;
-      if (cursorRef.current) {
-        cursorRef.current.style.transform = `translate3d(${clientX - 10}px, ${
-          clientY - 10
-        }px, 0)`;
-      }
-    };
 
-    window.addEventListener("mousemove", moveCursor);
-    return () => window.removeEventListener("mousemove", moveCursor);
-  }, []);
-
-  if (!mounted) return null; // prevent mismatch during hydration
-
-  return (
-    <div
-      ref={cursorRef}
-      className={`fixed top-0 left-0 z-[9999] w-7 h-7 rounded-full border-2 ${
-        resolvedTheme === "light" ? "border-[black]" : "border-[white]"
-      } opacity-60 pointer-events-none transition-transform duration-75`}
-    />
-  );
+    return (
+        <div
+            ref={cursorRef}
+            className={`fixed top-0 left-0 z-[9999] w-7 h-7 rounded-full border-2 ${theme === 'light' ? "border-[white]" : "border-[black]"} opacity-60 pointer-events-none transition-transform duration-75`}
+        />
+    );
 };
 
 export default CustomCursor;
--- src/app/layout.js
+++ src/app/layout.js
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
 
           {children}
         </ThemeProvider>
-        {/* <CustomCursor/> */}
+        <CustomCursor/>
       </body>
     </html>
   );
